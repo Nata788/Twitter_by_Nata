981596bac47dd00eb559f5ed711b34eec040b4c0
Nata788 <Nata788@gmail.com>
2017-04-17 14:51:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,26 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+TwitterByNata::Application.config.secret_key_base = '7d80320fe04d56c0f4c3731bd2d2a1e8bdf96bd44810c221a3ba8af7473fd64a618f94ce9c445d0e089ba8f2eb6b810bfb3f97d29be9f27591308bb18bccd201'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterByNata::Application.config.session_store :cookie_store, key: '_Twitter_by_Nata_session'
